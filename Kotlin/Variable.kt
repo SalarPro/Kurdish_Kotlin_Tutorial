d153903898f8e92e1760106ff48055a1e64aa02c
Salar Pro <salarkhalid97@gmail.com>
2020-04-14 17:36:50 +0300

    second lec

---
+++ Kotlin/Variable.kt
@@ -0,0 +1,33 @@
+fun main(args: Array<String>) {
+
+    var name: String
+    name = "Hello World"
+
+    println(name)
+
+    name = "Salar Pro"
+
+    println(name)
+
+    var age = 25
+
+
+    println(age)
+
+    var height: Double = 1.75
+
+    println(height)
+
+
+    var a: Int
+
+    a = 15
+
+    val username = "salar_pro"
+
+    var username2 = username
+
+    println(username2)
+
+
+}
